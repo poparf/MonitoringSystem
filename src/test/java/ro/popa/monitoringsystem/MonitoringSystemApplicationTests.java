439fe4d3d0115eb00b68ee7675e6a9f1027813af
Robert <poparobert22@stud.ase.ro>
2024-12-24 00:50:36 +0200

    first commit

---
+++ src/test/java/ro/popa/monitoringsystem/MonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.popa.monitoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
